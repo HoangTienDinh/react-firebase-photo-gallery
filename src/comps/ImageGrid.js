1aaffa8167a3c19b9e7610e71995feb344062b34
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-31 11:14:15 -0700

    make image modal pop up with selected img

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import useFirestore from '../hooks/useFirestore'
+import React from "react";
+import useFirestore from "../hooks/useFirestore";
 
-export default function ImageGrid() {
-    const { docs } = useFirestore('images')
-    console.log(docs)
+export default function ImageGrid({ setSelectedImg }) {
+  const { docs } = useFirestore("images");
+  console.log(docs);
 
-    return (
-        <div className='img-grid'>
-            { docs && docs.map(doc => (
-                <div className='img-wrap' key={doc.id}>
-                    <img src={doc.url} alt='uploaded pic'></img>
-                </div>
-            ))}
-        </div>
-    )
+  return (
+    <div className="img-grid">
+      {docs &&
+        docs.map((doc) => (
+          <div
+            className="img-wrap"
+            key={doc.id}
+            onClick={() => setSelectedImg(doc.url)}
+          >
+            <img src={doc.url} alt="uploaded pic"></img>
+          </div>
+        ))}
+    </div>
+  );
 }
