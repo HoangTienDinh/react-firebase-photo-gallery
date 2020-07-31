c40d34b36a8a1a4d3ca7cdecacaeb148d31f0979
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-31 11:21:58 -0700

    finish Modal of images

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
-export default function Modal({ selectedImg }) {
-    return (
-        <div className='backdrop'>
-            <img src={selectedImg} alt='enlarged pic' />
-        </div>
-    )
+export default function Modal({ selectedImg, setSelectedImg }) {
+  const handleClick = (e) => {
+    if (e.target.classList.contains("backdrop")) {
+      setSelectedImg(null);
+    }
+  };
+
+  return (
+    <div className="backdrop" onClick={handleClick}>
+      <img src={selectedImg} alt="enlarged pic" />
+    </div>
+  );
 }
