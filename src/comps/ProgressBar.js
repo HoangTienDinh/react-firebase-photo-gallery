bbfc2064a844c6a58120a6004b3e5c69a64e8b01
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-31 10:24:47 -0700

    create progressbar

---
+++ src/comps/ProgressBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import useStorage from "../hooks/useStorage";
+
+
+export default function ProgressBar() {
+  return (
+  <div className='progress-bar'>
+      progress
+
+  </div>
+  );
+}
