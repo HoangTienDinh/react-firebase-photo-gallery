8cb40653f410c03cb43b56d3e59646dc0f6df9f5
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-30 23:19:05 -0700

    start building useStorage hook

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,3 +1,14 @@
-import { useState, useEffect } from 'react';
-import { projectStorage } from '../firebase/config';
+import { useState, useEffect } from "react";
+import { projectStorage } from "../firebase/config";
 
+const useStorage = (file) => {
+  const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(null);
+  const [url, setUrl] = useState(null);
+
+  useEffect = (() => {
+
+  }, [file])
+
+
+};
