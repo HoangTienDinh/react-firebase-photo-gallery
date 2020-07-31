1182b3c4c4673360c502cfc8436df20f4b97d241
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-31 11:29:16 -0700

    add motion animation

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useFirestore from "../hooks/useFirestore";
+import { motion } from 'framer-motion';
 
 export default function ImageGrid({ setSelectedImg }) {
   const { docs } = useFirestore("images");
@@ -9,13 +10,19 @@ export default function ImageGrid({ setSelectedImg }) {
     <div className="img-grid">
       {docs &&
         docs.map((doc) => (
-          <div
+          <motion.div
             className="img-wrap"
             key={doc.id}
+            layout
+            whileHover={{ opacity: 1 }}
             onClick={() => setSelectedImg(doc.url)}
           >
-            <img src={doc.url} alt="uploaded pic"></img>
-          </div>
+            <motion.img src={doc.url} alt="uploaded pic" 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1 }}
+            />
+          </motion.div>
         ))}
     </div>
   );
