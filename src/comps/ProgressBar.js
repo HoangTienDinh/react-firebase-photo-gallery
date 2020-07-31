7e151c60a33d1b71b08e45ce43b800695f38b476
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-31 10:34:06 -0700

    add css for progress

--- src/comps/ProgressBar.js
+++ src/comps/ProgressBar.js
@@ -4,11 +4,9 @@ import useStorage from "../hooks/useStorage";
 
 export default function ProgressBar({ file, setFile }) {
     const { url, progress } = useStorage(file);
-    console.log(progress, url)
 
   return (
-  <div className='progress-bar'>
-      progress
+  <div className='progress-bar' style={{ width: progress + '%' }}>
 
   </div>
   );
