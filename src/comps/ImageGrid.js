04b35b7bd94fd76c9a354092c3405bf082f71639
Hoang Dinh <hoangdinh90@gmail.com>
2020-07-31 10:56:16 -0700

    display all images in ImageGrid

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import useFirestore from '../hooks/useFirestore'
 
 export default function ImageGrid() {
+    const { docs } = useFirestore('images')
+    console.log(docs)
+
     return (
         <div className='img-grid'>
-            images
+            { docs && docs.map(doc => (
+                <div className='img-wrap' key={doc.id}>
+                    <img src={doc.url} alt='uploaded pic'></img>
+                </div>
+            ))}
         </div>
     )
 }
