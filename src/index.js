ee0acb570df0f19c3ee55e707a8f4ec2556a0003
Hoang Dinh <hoangdinh90@gmail.com>
2020-09-10 14:35:45 -0700

    revert changes

--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>JUST TESTING</h1>
-    {/* <App /> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
